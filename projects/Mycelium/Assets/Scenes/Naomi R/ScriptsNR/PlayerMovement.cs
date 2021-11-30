4b318cfaf2be2874f3cc1cc6f3dfedfd44e2e7e7
GREM <naomiruokamo@gmail.com>
2021-11-30 14:04:50 +0100

    Update PlayerMovement.cs
    player now moves with mouse clicks

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerMovement.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerMovement.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Tilemap map;
+    MouseInput mouseInput;
+    private Vector3 destination;
+    [SerializeField] private float movementSpeed; 
+
+
+    private void Awake()
     {
-        
+        mouseInput = new MouseInput();
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        mouseInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        mouseInput.Disable();
+    }
+
+    void Start()
+    {
+        destination = transform.position;
+        mouseInput.Mouse.MouseClick.performed += _ => MouseClick();
+    }
+
+    private void MouseClick()
+    {
+        Vector2 mousePosition = mouseInput.Mouse.MousePosition.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        
+        //makes sure we are clicking on a tile 
+        if (map.HasTile(gridPosition))
+        {
+            destination = mousePosition;
+        }
+    }
+    
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, destination) > 0.1f)
+        {
+            transform. position = Vector3.MoveTowards(transform.position, destination, movementSpeed * Time.deltaTime); 
+        }
     }
 }
