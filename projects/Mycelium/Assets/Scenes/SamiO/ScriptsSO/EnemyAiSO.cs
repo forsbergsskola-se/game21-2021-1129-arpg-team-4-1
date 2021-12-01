fbd3600d3e170f608e48fa69c26b6e83140fb6ab
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-12-01 12:16:11 +0100

    Redo of enemy system

--- projects/Mycelium/Assets/Scenes/SamiO/ScriptsSO/EnemyAiSO.cs
+++ projects/Mycelium/Assets/Scenes/SamiO/ScriptsSO/EnemyAiSO.cs
@@ -1,25 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using System.Collections;
 
-public class EnemyAiSO : MonoBehaviour
+public class EnemyAiSo : MonoBehaviour
 {
-  public float speed;
-  public Transform target;
-  public float minimumDistance;
 
-  private void Update()
-  {
-    if (Vector2.Distance(transform.position, target.position) > minimumDistance)
+    public Transform target;//set target from inspector instead of looking in Update
+    public float speed;
+    public float distance;
+    public float agroRange;
+
+    void Start()
     {
-      transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-    }
-   //else
-   // {
-      //attack
-    //}
 
-  }
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < agroRange) //Agro range
+        {  //rotate to look at the player
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self);//correcting the original rotation
+        }
+
+        if (Vector3.Distance(transform.position, target.position) < agroRange) //Agro range
+        {   //move towards the player
+            if (Vector3.Distance(transform.position, target.position) > distance)
+            {//move if distance from target is greater than distance
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+        }
+    }
 }
