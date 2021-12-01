3fa3117f90b2219a9325c4029b952e135264d3ba
GREM <naomiruokamo@gmail.com>
2021-12-01 12:30:35 +0100

    Testing waters for Tiled Tilemaps

---
+++ projects/Mycelium/Assets/SuperTiled2Unity/Import/Test_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Test_Tiles" tilewidth="128" tileheight="128" tilecount="4" columns="2">
+ <image source="Test_Tiles.png" width="256" height="256"/>
+</tileset>
