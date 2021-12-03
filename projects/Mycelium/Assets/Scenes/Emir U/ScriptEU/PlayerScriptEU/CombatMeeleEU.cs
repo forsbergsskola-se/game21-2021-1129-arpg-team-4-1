e1acfd4359dacce5f54dbafc1ea271b2123893da
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-12-03 14:20:50 +0100

    Combat Script Setup

--- projects/Mycelium/Assets/Scenes/Emir U/ScriptEU/PlayerScriptEU/CombatMeeleEU.cs
+++ projects/Mycelium/Assets/Scenes/Emir U/ScriptEU/PlayerScriptEU/CombatMeeleEU.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatMeeleEU : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CombatMeeleEU : MonoBehaviour {
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play Attack animation
+        //Detect enemies in the range of attack
+        //Damage them
     }
 }
