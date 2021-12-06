c83ad6f9c41199a4f3dc09cbe899727fb82ab7a8
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-12-06 09:38:03 +0100

    Health Bar Update

--- projects/Mycelium/Assets/Scenes/Nadine D/ScriptsND/PlayerND.cs
+++ projects/Mycelium/Assets/Scenes/Nadine D/ScriptsND/PlayerND.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerND : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarND healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+    void TakeDamage (int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
