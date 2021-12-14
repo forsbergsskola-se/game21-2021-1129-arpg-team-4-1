cc1b487f70931172658a803ea844da3714b6e89d
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2021-12-14 10:38:28 +0100

    Enemy now facing player when aggro

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/EnemyAggro.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/EnemyAggro.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class EnemyAggro : MonoBehaviour
 {
-    [SerializeField]
-    private Transform player;
-   
-    [SerializeField]
-    private float agroRange;
-    
-    [SerializeField]
-    private float moveSpeed;
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float agroRange;
+
+    [SerializeField] private float moveSpeed;
 
     public float minDistance;
 
     private Rigidbody2D rb2d;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
         float distToPlayer = Vector3.Distance(transform.position, player.position);
@@ -32,26 +30,44 @@ public class EnemyAggro : MonoBehaviour
             ChasePlayer();
         }
 
-        else
-        {
-            StopChasingPlayer();
-        }
+       // else
+       // {
+          //  StopChasingPlayer();
+       // }
     }
 
-    private void StopChasingPlayer()
-    {
-        rb2d.velocity = Vector3.zero;
+   // private void StopChasingPlayer()
+    //{
+       // rb2d.velocity = Vector3.zero;
 
-    }
+   // }
 
     private void ChasePlayer()
     {
-        if(Vector3.Distance(transform.position, player.position) > minDistance) {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-        }
-        else
+        if (Vector3.Distance(transform.position, player.position) < agroRange) //Agro range
         {
-            //Attack
+            //rotate to look at the player
+            transform.LookAt(player.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self); //correcting the original rotation
+        }
+
+        if (Vector3.Distance(transform.position, player.position) < agroRange) //Agro range
+        {
+            //move towards the player
+            if (Vector3.Distance(transform.position, player.position) > minDistance)
+            {
+                //move if distance from target is greater than distance
+                transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+            }
+
+            {
+                //Attack
+            }
         }
     }
 }
+
+
+//if(Vector3.Distance(transform.position, player.position) > minDistance) {
+   // transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+//}
