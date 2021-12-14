c81faabb8f7b60f59bcc2e7f105bf5566e66fe00
GREM <naomiruokamo@gmail.com>
2021-12-14 16:43:37 +0100

    Update CursorManagerNR.cs
    cleaning it up and adding more documentation

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CursorManagerNR.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CursorManagerNR.cs
@@ -6,10 +6,11 @@ using UnityEngine.Tilemaps;
 
 public class CursorManagerNR : MonoBehaviour
 {
-    public Texture2D cursor;
-    public Texture2D cursorInvalid;
     private CompositeCollider2D collider;
-    [SerializeField] private PlayerMovement PM;
+    [SerializeField] private PlayerMovementNR PM;
+    [SerializeField] private Texture2D cursor;
+    [SerializeField] private Texture2D cursorInvalid;
+    [SerializeField] private Texture2D cursorClicked;
 
 
     private void Awake()
@@ -17,18 +18,13 @@ public class CursorManagerNR : MonoBehaviour
         // takes in the Collider Tilemaps' Composite Collider 2D which the onMouse methods need.
         collider = GetComponent<CompositeCollider2D>();
         
+        //makes sure we start out with our default cursor
         ChangeCursor(cursor);
         
-        // keeps the cursor inside the game, click esc to escape the window.
+        // locks the cursor inside the game, click esc to escape the window.
         Cursor.lockState = CursorLockMode.Confined; 
     }
-
-    private void ChangeCursor(Texture2D cursorType)
-    {
-        Cursor.SetCursor(cursorType, new Vector2 (0, 0), CursorMode.Auto);
-    }
-
-   
+    
     private void OnMouseEnter()
     {
         // changes the mouse when it enters the collider, sets valid location to false since we're now over collider. 
@@ -49,5 +45,14 @@ public class CursorManagerNR : MonoBehaviour
         // changes the mouse when we leave the collider, sets the valid location to true since we're now outside of the colliders and should be able to move.
         ChangeCursor(cursor);
         PM.isValidLocation = true;
+    }
+    
+    private void ChangeCursor(Texture2D cursorType)
+    {
+        //the second argument is for knowing where we click from, with a normal cursor its the top left, with a middle one its the middle.
+        // in our case since the cursor is pointing to the top left we pass Vector2.zero as our argument. Alternatives to this are:
+        // new Vector2 (x: int, y: int) add numbers that fits your cursors point
+        // Vector2 hotspot = new Vector2(cursorType.width /2, cursorType.height /2) then pass hotspot in the second argument for a middle cursor. 
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
     }
 }
