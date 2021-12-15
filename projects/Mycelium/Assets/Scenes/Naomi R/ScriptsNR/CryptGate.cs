30beeb149b9e323e426a360f553367f13a41487f
GREM <naomiruokamo@gmail.com>
2021-12-15 15:37:19 +0100

    Create empty CryptGate script

---
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CryptGate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryptGate : MonoBehaviour, IClick
+{
+    public void onClickAction()
+    {
+        //add what it does on click
+    }
+}
