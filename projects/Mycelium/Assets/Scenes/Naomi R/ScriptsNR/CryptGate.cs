e4864b9df21b318331ef18354a97c46688d00fe4
GREM <naomiruokamo@gmail.com>
2021-12-16 21:29:23 +0100

    Trying to fix collisions and gates

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CryptGate.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CryptGate.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class CryptGate : MonoBehaviour, IClick
 {
+    [SerializeField] private PlayerCollisionNr PC;
+    
     public void onClickAction()
     {
-        Debug.Log("You clicked the gate");
-        Destroy(gameObject);
+        if (PC.isClickColliding)
+        {
+            Debug.Log("you clicked gate");
+            Destroy(gameObject);
+        }
+        else Debug.Log("Not Colliding");
     }
+    
 }
