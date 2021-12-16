6fe8be1aff1410e324fb5ad87fe056e73566ade3
GREM <naomiruokamo@gmail.com>
2021-12-16 21:49:23 +0100

    Update PlayerCollisionNr.cs

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerCollisionNr.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerCollisionNr.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCollisionNr : MonoBehaviour
+public class PlayerCollisionNR : MonoBehaviour
 {
     public bool isClickColliding;
     
