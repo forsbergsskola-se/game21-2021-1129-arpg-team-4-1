a8464da9cefef0f2d70b8c2a5d4f396090d1d337
deuimint <denisejoyy@icloud.com>
2021-12-16 16:31:18 +0100

    minor changes for test

--- projects/Mycelium/Assets/Scenes/Denise J/gamemanager.cs
+++ projects/Mycelium/Assets/Scenes/Denise J/gamemanager.cs
@@ -7,13 +7,13 @@ namespace Scenes.Denise_J
     {
         public GameObject damageTextPrefab, enemyInstance;
 
-        public string textToDisplay;
+        public string damageTaken;
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.X))
             {
                 GameObject damageTextInstance = Instantiate(damageTextPrefab, enemyInstance.transform);
-                damageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(textToDisplay);
+                damageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(damageTaken);
             }
         }
     }
