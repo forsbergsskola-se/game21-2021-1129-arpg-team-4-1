0e583e6b9f0064dbffed34f157b3cb7cc600e6f2
GREM <naomiruokamo@gmail.com>
2021-12-16 21:49:29 +0100

    Update CryptGate.cs

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CryptGate.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/CryptGate.cs
@@ -6,12 +6,15 @@ using UnityEngine.SocialPlatforms;
 
 public class CryptGate : MonoBehaviour, IClick
 {
-    [SerializeField] private PlayerCollisionNr PC;
+    [SerializeField] private PlayerCollisionNR PC;
+    [SerializeField] private PlayerMovementNR PM;
+    [SerializeField] private GameObject Player;
     
     public void onClickAction()
     {
         if (PC.isClickColliding)
         {
+            PM.destination = Player.transform.position;
             Debug.Log("you clicked gate");
             Destroy(gameObject);
         }
