2e02790547c50c15371bf0b6f90fde4cf9030277
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-12-17 12:11:34 +0100

    Starting with clicking movement animation

---
+++ projects/Mycelium/Assets/Scenes/Emir U/ScriptEU/PlayerScriptEU/IndieMovementEU.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndieMovementEU : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D Rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
