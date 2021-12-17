2bf74c5e7b133fce29e6f617b59fde024dd2087a
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-12-17 12:51:38 +0100

    Change of direction animation movement done
    i havent done clicking function movement but the change of direction movement animation is done

--- projects/Mycelium/Assets/Scenes/Emir U/ScriptEU/PlayerScriptEU/IndieMovementEU.cs
+++ projects/Mycelium/Assets/Scenes/Emir U/ScriptEU/PlayerScriptEU/IndieMovementEU.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,25 @@ public class IndieMovementEU : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D Rb;
+    public Animator animator;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
+        // Input 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        Rb.MovePosition(Rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
