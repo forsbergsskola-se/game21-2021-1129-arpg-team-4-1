ebd549475ede30e94741aae644b4cc46e12ab5e6
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-12-31 00:03:32 +0100

    Adding comments to Player Script :pencil2:

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/Player.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/Player.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public int maxHealth = 100;
-    public int currentHealth;
-
-    public PlayerHealthBar healthBar;
+    public int maxHealth = 100; //Player max health is 100
+    public int currentHealth; //Player current health
+    public PlayerHealthBar healthBar; //Reference to player health bar
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;
+        currentHealth = maxHealth; //When we start our game, players health will be set to max health
         healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
+    //Everytime we press the space bar, player takes 20 damage
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -28,8 +27,7 @@ public class Player : MonoBehaviour
 
     void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-
+        currentHealth -= damage; //When taking damage, we subtract damage from players current health
         healthBar.SetHealth(currentHealth);
     }
 }
