1649129b961c7dc3c91bd54634a98066f5e1e476
Nadia Dahlberg <nadiadahlberg@outlook.com>
2021-12-31 00:50:06 +0100

    Update PlayerHealthBar.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/PlayerHealthBar.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/PlayerHealthBar.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //Adding correct namespace. This allowes us to create a variabel that lets us store our slider.
+
 public class PlayerHealthBar : MonoBehaviour
 {
+//These functions are public so we can call them from other scripts.
     public Slider slider;
     public Gradient gradient;
     public Image fill;
@@ -15,7 +17,7 @@ public class PlayerHealthBar : MonoBehaviour
     }
     public void SetHealth(int health)
     {
-        slider.value = health;
+        slider.value = health; 
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
