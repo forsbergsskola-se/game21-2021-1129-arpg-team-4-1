3cf17f009e1fb2982c330f782ecb7cfbb08bbecd
GREM <naomiruokamo@gmail.com>
2022-01-03 12:51:12 +0100

    removes my Tiled folder

--- projects/Mycelium/Assets/Scenes/Naomi R/MaterialNR/Tiled/tiles_crypt_256.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tiles_crypt_256" tilewidth="256" tileheight="256" tilecount="16" columns="4">
- <image source="tiles_crypt_256.png" width="1024" height="1024"/>
-</tileset>
