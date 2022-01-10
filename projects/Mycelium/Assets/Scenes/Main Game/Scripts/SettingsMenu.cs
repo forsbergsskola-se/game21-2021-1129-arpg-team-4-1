7eab01ec8786f2ad3b0ce13caf5fcf39f0944d9e
Nadia Dahlberg <nadiadahlberg@outlook.com>
2022-01-10 09:59:25 +0100

    Update SettingsMenu.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/SettingsMenu.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/SettingsMenu.cs
@@ -19,10 +19,17 @@ resolutionDropdown.ClearOptions();
 
 List <string> options = new List <string>();
 
+int currentResolutionIndex = 0;
+
 for (int i = 0; i < resolutions.Length; i++)
 {
 string option = resolutions[i].width + " x " + resolutions[i].height;
 options.Add(option);
+
+if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+{
+currentResolutionIndex = i;
+}
 }
 
 resolutionDropdown.AddOptions(options);
