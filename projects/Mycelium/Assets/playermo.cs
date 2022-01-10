d41234fbb6a1e9d4dbb5a8d8ddf59f6cc4d2d13f
ojojboll <olof.forsgren@student.forsbergsskola.se>
2022-01-10 14:37:37 +0100

    added animations

---
+++ projects/Mycelium/Assets/playermo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermo : MonoBehaviour
+{
+
+    public float movespeed = 5f;
+
+    public rigidbody2d rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
