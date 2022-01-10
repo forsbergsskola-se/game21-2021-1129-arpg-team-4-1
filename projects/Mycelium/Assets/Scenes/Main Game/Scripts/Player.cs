7b7ef1217b0cdc2a6687f7fdf00c84b03df3cf4d
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-10 13:11:05 +0100

    Update Player.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/Player.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/Player.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform enemy;
-    [SerializeField] private float attackTimer;
+
+    
     public static int maxHealth = 100; //Player max health is 100
     public static int currentHealth; //Player current health
     public PlayerHealthBar healthBar; //Reference to player health bar 
     public static bool gameOver;
-    public float minDistance;
-    private float attackCooldown = 2.5f;
-    public float attackRange = 0f;   
+   
 
     void Start()
     {
         currentHealth = maxHealth; //When we start our game, players health will be set to max health
         healthBar.SetMaxHealth(maxHealth);
-        attackRange = minDistance + 0.2f;
+       
         gameOver = false; 
     }
 
@@ -36,18 +34,8 @@ public class Player : MonoBehaviour
             gameOver = true;
         } 
         
-        if (attackTimer > 0)
-        {
-            attackTimer -= Time.deltaTime;
-            //Debug.Log("attacktimer"+ attackTimer);
-        } 
-        float distToPlayer = Vector3.Distance(transform.position, enemy.position);
-       
-        if (distToPlayer <= attackRange)
-        {
-            Attack();
-            
-        }
+     
+
     }
 
     public void TakeDamage(int damage)
@@ -62,21 +50,6 @@ public class Player : MonoBehaviour
         }
        
     }
-    private void Attack()
-    {
-        if (attackTimer > 0)
-        {
-            return;
-        }
-        Debug.Log("Attack");
-        Player playerHealth = enemy.GetComponent<Player>();
-        if (playerHealth != null)
-        {
-            playerHealth.TakeDamage(10);
-            //anime here
-            Debug.Log("Hit player");
-            attackTimer = attackCooldown;
-        }
-    }
+
    
 }
