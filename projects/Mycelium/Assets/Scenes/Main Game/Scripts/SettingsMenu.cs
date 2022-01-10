4907f779e18554871d73bf00af1d56285c17e264
Nadia Dahlberg <nadiadahlberg@outlook.com>
2022-01-10 03:49:31 +0100

    Update SettingsMenu.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/SettingsMenu.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/SettingsMenu.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio; //To reference a Audio Mixer we need to use the Unity Engine Audio System
 
 public class SettingsMenu : MonoBehaviour {
 
+public AudioMixer audioMixer;
+
 public void SetVolume (float volume)
 {
-Debug.Log(volume);
+audioMixer.SetFloat("Volume", volume);
 }
 
 }
