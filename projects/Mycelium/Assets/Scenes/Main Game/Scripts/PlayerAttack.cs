3ed973f223367b7fab4d7623c9554762e1d4a8ad
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-10 21:00:10 +0100

    Player attack

---
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/PlayerAttack.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyTargeted;
+    //[SerializeField] private enemyHealthScirpt enemyHealth;
+    [SerializeField] private float attackRange = 5f;
+    [SerializeField] private float attackTimer = 0.0f;
+    
+    private float attackCooldown = 2.5f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (enemyTargeted == null)
+        {
+            return;
+        }
+        float distToEnemy = Vector3.Distance(transform.position, enemyTargeted.transform.position);
+        
+        if (distToEnemy <= attackRange)
+        {
+            Attack();
+        } 
+        
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+            //Debug.Log("attacktimer"+ attackTimer);
+        }
+    }
+
+    public void SetEnemyTarget(GameObject newTarget)
+    {
+        if (newTarget == null)
+        {
+            return;
+        }
+        enemyTargeted = newTarget; 
+        //enemyHealth = enemyTargeted.GetComponent<enemyHealthScript>();
+        
+    } 
+    
+    private void Attack()
+    {
+        if (attackTimer > 0)
+        {
+            return;
+        }
+        Debug.Log("Attack enemy");
+        attackTimer = attackCooldown;
+        //change to enemy health
+        
+        //if (enemyHealth != null)
+        //{
+          //  enemyHealth.TakeDamage(10);
+            //anime here
+           // Debug.Log("Hit enemy");
+           // attackTimer = attackCooldown;
+        //}
+    }
+}
