904dc398a9b3a69a7265ba5ca3b5f1e337f69ad9
GREM <naomiruokamo@gmail.com>
2022-01-11 14:00:53 +0100

    Update PlayerMovementExperimentation.cs

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerMovementExperimentation.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/PlayerMovementExperimentation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,58 +6,74 @@ using UnityEngine.Tilemaps;
 
 public class PlayerMovementExperimentation : MonoBehaviour
 { 
-    public Vector3 destination;
+    public CursorControllerNR CC;
+    private Vector3 destination;
     private GameObject currentPrefab;
     private bool hasSpawned;
-    public CursorControllerNR CC;
-   
+
     [SerializeField] private MapCollision MC;
     [SerializeField] private GameObject player;
     [SerializeField] private float movementSpeed;
     [SerializeField] private GameObject prefab;
     [SerializeField] private Tilemap map;
-       
-       
+    [SerializeField] private Animator animator;
+    
+    
     void Start()
     {
         destination = player.transform.position;
         CC.controls.Mouse.MouseClick.performed += _ => MouseClick();
     }
-       
+    
     void Update()
     {
         player.transform.position = Vector3.MoveTowards(player.transform.position, destination, movementSpeed * Time.deltaTime); 
-           
+        
+        animator.SetFloat("Horizontal", destination.x);
+        animator.SetFloat("Vertical", destination.y);
+        SpeedFix();
+        
         if (DestinationReached() && hasSpawned)
         {
             Destroy(currentPrefab);
             hasSpawned = false;
         }
     }
-   
+
+    private void SpeedFix()
+    {
+        if (DestinationReached())
+        {
+            animator.SetFloat("Speed", 0);
+        }
+        else animator.SetFloat("Speed", destination.sqrMagnitude);
+        
+    }
+
     private void MouseClick()
     {
         Vector2 mousePosition = CC.controls.Mouse.MousePosition.ReadValue<Vector2>();
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector3Int gridPosition = map.WorldToCell(mousePosition);
-           
-           
+
+
         if (map.HasTile(gridPosition) && MC.isValidLocation)
         {
             destination = mousePosition;
             SpawnPointer();
         }
     }
-   
+
     private void SpawnPointer()
     {
         Destroy(currentPrefab);
         currentPrefab = Instantiate(prefab, destination, Quaternion.identity);
         hasSpawned = true;
     }
-       
+    
     private bool DestinationReached()
     {
         return destination == player.transform.position;
     }
+    
 }
