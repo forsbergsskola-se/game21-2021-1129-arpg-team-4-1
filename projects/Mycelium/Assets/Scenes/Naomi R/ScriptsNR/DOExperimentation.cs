081e3ee7ce6fc306e548958b73459c6d8ccbc6ef
GREM <naomiruokamo@gmail.com>
2022-01-11 22:13:14 +0100

    Update DOExperimentation.cs

--- projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/DOExperimentation.cs
+++ projects/Mycelium/Assets/Scenes/Naomi R/ScriptsNR/DOExperimentation.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DOExperimentation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isObject;
+    
+    private void FixedUpdate()
     {
-        
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+    
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other != null && other.tag == "Barrel")
+        {
+            isObject = true;
+        }
+        else isObject = false;
     }
 }
