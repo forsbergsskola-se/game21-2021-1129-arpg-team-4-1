f552ef5f7b95df8eec82871750c362097dd5df4c
GREM <naomiruokamo@gmail.com>
2022-01-11 16:10:13 +0100

    Update TeleportStones.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/TeleportStones.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/TeleportStones.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,27 @@ public class TeleportStones : MonoBehaviour, IClick
 {
     [SerializeField] private PlayerCollisionNR PC;
     [SerializeField] private GameObject Player;
+    [SerializeField] private PlayerMovementExperimentation PM;
     [SerializeField] private GameObject NextStatue;
     [SerializeField] private Vector3 offset;
+    private bool hasTeleported;
 
     public void onClickAction()
     {
         if (PC.isClickColliding)
         {
             Player.transform.position = NextStatue.transform.position + offset;
+            hasTeleported = true;
         }
         else Debug.Log("Not close enough");
     }
 
+    public void FixedUpdate()
+    {
+        if (hasTeleported)
+        {
+            PM.destination = NextStatue.transform.position + offset;
+            hasTeleported = false;
+        }
+    }
 }
