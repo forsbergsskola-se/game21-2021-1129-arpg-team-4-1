5192fe8c83d8d4583ce5440beb593f4ed600ae8f
GREM <naomiruokamo@gmail.com>
2022-01-12 02:28:10 +0100

    Update MainMenu.cs
    now it uses the level loader

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/MainMenu.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/MainMenu.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement; //Used when switching scenes in Unity
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private LevelLoader LL;
 
-public void PlayGame () //We need to make the method public, in order to call it from the button
-{
-SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-}
+    public void PlayGame () //We need to make the method public, in order to call it from the button
+    {
+       LL.LoadNextLevel();
+    }
 
-public void QuitGame () //This will close down the program when pressing the "Quit"-button in Main Menu
-{
-Debug.Log ("QUIT!"); //Unity Ediotor will not close the program, instead this message will let us know that this is working
-Application.Quit();
-}
+    public void QuitGame () //This will close down the program when pressing the "Quit"-button in Main Menu
+    {
+        Debug.Log ("QUIT!"); //Unity Editor will not close the program, instead this message will let us know that this is working
+        Application.Quit();
+    }
 }
