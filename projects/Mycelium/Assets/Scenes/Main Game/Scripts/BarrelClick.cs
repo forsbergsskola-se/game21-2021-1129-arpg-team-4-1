7c3f0ae3ace63d660197a2f4a91190b8df3e822a
GREM <naomiruokamo@gmail.com>
2022-01-12 03:37:24 +0100

    Update BarrelClick.cs

--- projects/Mycelium/Assets/Scenes/Main Game/Scripts/BarrelClick.cs
+++ projects/Mycelium/Assets/Scenes/Main Game/Scripts/BarrelClick.cs
@@ -6,15 +6,18 @@ public class BarrelClick : MonoBehaviour ,IClick
 {
     private GameObject player;
     private PlayerAttack attackScript;
+    private Animator playerAnimator;
     
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         attackScript = player.GetComponent<PlayerAttack>();
+        playerAnimator = player.gameObject.GetComponent<Animator>();
     }
 
     public void onClickAction()
     {
+        playerAnimator.SetBool("isAttacking", true);
         attackScript.SetEnemyTarget(this.gameObject);
         Debug.Log("Barrel attacked");
     }
